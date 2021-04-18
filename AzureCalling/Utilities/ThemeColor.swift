706f1a0d734613baa37c18ff7447c68c2c861ced
Amy Long <longamy@microsoft.com>
2021-04-18 10:05:14 -0700

    Remove fallback color

--- AzureCalling/Utilities/ThemeColor.swift
+++ AzureCalling/Utilities/ThemeColor.swift
@@ -7,7 +7,7 @@ import Foundation
 import UIKit
 
 enum ThemeColor {
-    static var primary = UIColor(named: "PrimaryColor") ?? UIColor(red: 0, green: 120 / 255, blue: 212 / 255, alpha: 1)
+    static var primary = UIColor(named: "PrimaryColor")!
 
-    static var gray300 = UIColor(named: "Gray300") ?? UIColor(red: 172 / 255, green: 172 / 255, blue: 172 / 255, alpha: 1)
+    static var gray300 = UIColor(named: "Gray300")!
 }
