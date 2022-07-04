1452e22fc45d671b8510d4313e3aaff0d6e4da5b
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2022-07-04 11:09:54 -0700

    Clarify constraints code

--- AzureCalling/Utilities/LayoutHelpers.swift
+++ AzureCalling/Utilities/LayoutHelpers.swift
@@ -7,112 +7,81 @@ import UIKit
 
 extension UIView {
     func expandHorizontallyInSuperView(withEqualMargin offset: CGFloat = 0) {
-        guard self.superview != nil else {
+        guard let superView = self.superview else {
             return
         }
         translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .leading,
-                               relatedBy: .equal,
-                               toItem: superview, attribute: .leading,
-                               multiplier: 1, constant: offset),
-            NSLayoutConstraint(item: self, attribute: .trailing,
-                               relatedBy: .equal,
-                               toItem: superview, attribute: .trailing,
-                               multiplier: 1, constant: -offset)
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: offset),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -offset)
         ])
     }
 
     func pinToBottom(withMargin offset: CGFloat = 8) {
-        guard self.superview != nil else {
+        guard let superView = self.superview else {
             return
         }
         translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .bottom,
-                               relatedBy: .equal,
-                               toItem: superview?.safeAreaLayoutGuide, attribute: .bottom,
-                               multiplier: 1, constant: -offset)
+            bottomAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: -offset)
         ])
     }
 
     func pinToTop(withMargin offset: CGFloat = 8) {
-        guard self.superview != nil else {
+        guard let superView = self.superview else {
             return
         }
         translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .top,
-                               relatedBy: .equal,
-                               toItem: superview?.safeAreaLayoutGuide, attribute: .top,
-                               multiplier: 1, constant: offset)
+            topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.topAnchor, constant: offset)
         ])
     }
 
     func pinToLeft(withMargin offset: CGFloat = 8) {
-        guard self.superview != nil else {
+        guard let superView = self.superview else {
             return
         }
         translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .left,
-                               relatedBy: .equal,
-                               toItem: superview, attribute: .left,
-                               multiplier: 1, constant: offset)
+            leftAnchor.constraint(equalTo: superView.leftAnchor, constant: offset)
         ])
     }
 
     func pinToRight(withMargin offset: CGFloat = 8) {
-        guard self.superview != nil else {
+        guard let superView = self.superview else {
             return
         }
         translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .right,
-                               relatedBy: .equal,
-                               toItem: superview, attribute: .right,
-                               multiplier: 1, constant: -offset)
+            rightAnchor.constraint(equalTo: superView.rightAnchor, constant: -offset)
         ])
     }
 
     func centerVerticallyInContainer(offset: CGFloat = 0) {
-        guard self.superview != nil else {
+        guard let superView = self.superview else {
             return
         }
         translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal,
-                               toItem: superview, attribute: .centerY,
-                               multiplier: 1, constant: offset)
+            centerYAnchor.constraint(equalTo: superView.centerYAnchor, constant: offset)
         ])
     }
 
     func centerHorizontallyInContainer(offset: CGFloat = 0) {
-        guard self.superview != nil else {
+        guard let superView = self.superview else {
             return
         }
         translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal,
-                               toItem: superview?.safeAreaLayoutGuide, attribute: .centerX,
-                               multiplier: 1, constant: offset)
+            centerXAnchor.constraint(equalTo: superView.centerXAnchor, constant: offset)
         ])
     }
 
     func fixSizeTo(size: CGSize) {
         translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal,
-                               toItem: nil, attribute: .width,
-                               multiplier: 1, constant: size.width),
-            NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal,
-                               toItem: nil, attribute: .height,
-                               multiplier: 1, constant: size.height)
+            widthAnchor.constraint(equalToConstant: size.width),
+            heightAnchor.constraint(equalToConstant: size.height)
         ])
     }
 }
