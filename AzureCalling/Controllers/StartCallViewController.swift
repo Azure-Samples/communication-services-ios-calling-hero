9ea93490ad6a3dcbe27ac3a0dfdb6eaebb81400a
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2022-07-13 20:23:35 -0700

    Switch to new keyboard handling

--- AzureCalling/Controllers/StartCallViewController.swift
+++ AzureCalling/Controllers/StartCallViewController.swift
@@ -6,19 +6,40 @@
 import UIKit
 import FluentUI
 
-class StartCallViewController: UIViewController, UITextFieldDelegate {
+class StartCallViewController: UIViewController {
+
+    // MARK: Constants
+    private let maxDisplayNameSize: Int = 256
 
     // MARK: Properties
+    private var contentView: UIView!
     private var displayNameStackView: UIStackView!
     private var displayNameLabel: FluentUI.Label!
     private var captionLabel: FluentUI.Label!
     private var displayNameTextField: IconTextField!
     private var nextButton: FluentUI.Button!
 
+    // Handle keyboard scrolling the content
+    private var bottomConstraint: NSLayoutConstraint!
+    private var keyboardObserver: NSObjectProtocol!
+
     // MARK: ViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        view.backgroundColor = ThemeColor.lightSurfacesPrimary
+        title = "Start call"
+
+        contentView = setupUI()
+
+        keyboardObserver = NotificationCenter.default.addObserver(
+            forName: UIResponder.keyboardWillChangeFrameNotification,
+            object: nil, queue: .main,
+            using: handleKeyboardNotification(notification:)
+        )
+
+        // Dismiss keyboard if tapping outside
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -26,25 +47,108 @@ class StartCallViewController: UIViewController, UITextFieldDelegate {
         displayNameTextField.becomeFirstResponder()
     }
 
-    // MARK: UI layout
-    private func setupUI() {
-        view.backgroundColor = ThemeColor.lightSurfacesPrimary
-        self.navigationItem.title = "Start call"
+    deinit {
+        displayNameTextField?.delegate = nil
+        NotificationCenter.default.removeObserver(keyboardObserver as Any)
+     }
+
+    // MARK: Private Functions
+    private func handleKeyboardNotification(notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let timing = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber,
+              let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber,
+              let frameEnd = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let window = view.window else {
+            return
+        }
+
+        let endRect = frameEnd.cgRectValue
+        let options = UIView.AnimationOptions(rawValue: curve.uintValue << 16)
+        let duration = timing.doubleValue
+
+        // Get keyboard height.
+        let keyboardFrame = view.convert(endRect, from: window)
+        let intersection = view.bounds.intersection(keyboardFrame)
+        let keyboardHeight = intersection.isNull ? 0 : intersection.size.height
+
+        UIView.animate(withDuration: duration, delay: 0, options: options) { [weak self] in
+            let bottomInset = keyboardHeight > 0 ? keyboardHeight : self?.view.safeAreaInsets.bottom
+            self?.bottomConstraint?.constant = -(bottomInset ?? 0)
+            self?.view.layoutIfNeeded()
+        }
+    }
+
+    // MARK: Action Handling
+    private func onNextButtonTapped() {
+        let inviteVc = UIViewController()
+        navigationController?.pushViewController(inviteVc, animated: true)
+    }
+}
+
+extension StartCallViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+
+        if let updatedString = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) {
+            // Display name is not allowed to be too long
+            if updatedString.lengthOfBytes(using: .utf8) > maxDisplayNameSize {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+// MARK: - UI Layout
+private extension StartCallViewController {
+    private func setupUI() -> UIView {
+        let content = UIView()
+        content.translatesAutoresizingMaskIntoConstraints = false
+
         setupLabels()
         setupDisplayNameTextField()
         setupNextButton()
-        layoutTextFieldContainer()
-        layoutMainContainer()
+
+        let formStackContainer = setupForm()
+        content.addSubview(formStackContainer)
+        formStackContainer.expandHorizontallyInSuperView()
+        formStackContainer.pinToTop()
+
+        content.addSubview(nextButton)
+        nextButton.expandHorizontallyInSuperView(withEqualMargin: 16)
+        nextButton.pinToBottom(withMargin: 16)
+
+        // Add content to a scrollview
+        let scroller = content.wrapInScrollview()
+        view.addSubview(scroller)
+        scroller.translatesAutoresizingMaskIntoConstraints = false
+
+        bottomConstraint = scroller.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+
+        NSLayoutConstraint.activate([
+            scroller.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bottomConstraint,
+            scroller.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            scroller.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+
+        return content
     }
 
     private func setupLabels() {
-        displayNameLabel = FluentUI.Label(style: .footnote)
-        displayNameLabel.text = "Display name"
-        displayNameLabel.textColor = FluentUI.Colors.textSecondary
+        displayNameLabel = FluentUI.Label.createWith(style: .footnote,
+                                                      colorStyle: .secondary,
+                                                      value: "Display name")
 
-        captionLabel = FluentUI.Label(style: .caption1)
-        captionLabel.text = "Name shown the others on the call."
-        captionLabel.textColor = FluentUI.Colors.textSecondary
+        captionLabel = FluentUI.Label.createWith(style: .caption1,
+                                                 colorStyle: .secondary,
+                                                 value: "Name shown the others on the call.")
     }
 
     private func setupDisplayNameTextField() {
@@ -52,14 +156,9 @@ class StartCallViewController: UIViewController, UITextFieldDelegate {
         displayNameTextField.placeholder = "Enter a name"
         displayNameTextField.image = UIImage(named: "ic_fluent_person_20_regular")
         displayNameTextField.imageSize = CGSize(width: 16, height: 20)
-
+        displayNameTextField.keyboardType = .asciiCapable
+        displayNameTextField.autocorrectionType = .no
         displayNameTextField.delegate = self
-        // Dismiss keyboard if tapping outside
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
-        view.addGestureRecognizer(tap)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     private func setupNextButton() {
@@ -70,79 +169,19 @@ class StartCallViewController: UIViewController, UITextFieldDelegate {
         )
     }
 
-    private func layoutTextFieldContainer() {
-        let displayNameLabelContainer = containerWithHorizontalEqualMargin(contentView: displayNameLabel)
-        displayNameLabel.pinToBottom(withMargin: 4)
-        let captionLabelContainer = containerWithHorizontalEqualMargin(contentView: captionLabel)
-
-        displayNameTextField.fixHeightTo(height: 48)
-
-        displayNameStackView = UIStackView(arrangedSubviews: [displayNameLabelContainer, displayNameTextField, captionLabelContainer])
-
-        displayNameStackView.axis = .vertical
-        displayNameStackView.alignment = .fill
-        displayNameStackView.spacing = 4
-        view.addSubview(displayNameStackView)
-
-        displayNameStackView.expandHorizontallyInSuperView(withEqualMargin: 0)
-        displayNameStackView.pinToTop()
+    private func setupForm() -> UIStackView {
+        let formStack = UIStackView(arrangedSubviews: [
+            PaddingContainer(
+                containing: displayNameLabel,
+                padding: UIEdgeInsets(top: 24, left: 16, bottom: 8, right: 16)
+            ),
+            displayNameTextField,
+            PaddingContainer(
+                containing: captionLabel,
+                padding: UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+            )
+        ])
+        formStack.axis = .vertical
+        return formStack
     }
-
-    private func layoutMainContainer() {
-        let buttonContainer = containerWithHorizontalEqualMargin(contentView: nextButton)
-
-        let stackView = UIStackView(arrangedSubviews: [self.displayNameStackView, buttonContainer])
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.distribution = .equalCentering
-        view.addSubview(stackView)
-
-        stackView.expandHorizontallyInSuperView(withEqualMargin: 0)
-        stackView.pinToTop(withMargin: 24)
-        stackView.pinToBottom(withMargin: 16)
-
-    }
-
-    private func containerWithHorizontalEqualMargin(contentView: UIView, withEqualMargin: CGFloat = 16) -> UIView {
-        let container = UIView()
-        container.addSubview(contentView)
-        contentView.expandHorizontallyInSuperView(withEqualMargin: withEqualMargin)
-        contentView.expandVerticallyInSuperView(withEqualMargin: 0)
-        return container
-    }
-
-    // MARK: Action Handling
-    private func onNextButtonTapped() {
-        let inviteVc = UIViewController()
-        navigationController?.pushViewController(inviteVc, animated: true)
-    }
-
-    // MARK: UITextFieldDelegate
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let currentText = textField.text ?? ""
-        guard let stringRange = Range(range, in: currentText) else {
-            return false
-        }
-
-        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
-        return updatedText.count <= 256
-    }
-
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue, view.frame.size.height == UIScreen.main.bounds.height {
-            view.frame.size.height -= keyboardSize.height
-        }
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if view.frame.size.height != UIScreen.main.bounds.height {
-            view.frame.size.height = UIScreen.main.bounds.height
-        }
-    }
-
 }
